0c8e54f34e73673913a74773b717c6bba30114dc
Sergio Aciar <aciarenator@gmail.com>
2023-08-07 10:55:04 -0300

    prueba con osascript con extension scpt

--- src/main/java/view/SpeakerPreview.java
+++ src/main/java/view/SpeakerPreview.java
@@ -484,7 +484,7 @@ public class SpeakerPreview extends JDialog {
 					String rutaArchivo =PropertiesManager.getInstance().getProperty("data")+"/Octopus/data/"+sp.getNamePresentation().substring(0,sp.getNamePresentation().indexOf("."))+"/"+sp.getNamePresentation();
 					System.out.println("Abriendooo -----------------"+rutaArchivo);
 					
-					Runtime.getRuntime().exec(new String[] {"open", "osascript", "/Users/congressrental/Desktop/script.scpt",rutaArchivo });
+					Runtime.getRuntime().exec(new String[] {"osascript", "/Users/congressrental/Desktop/script.scpt",rutaArchivo });
 					
 					//Runtime.getRuntime().exec("open osascript /Users/congressrental/Desktop/script.scpt "+rutaArchivo );
 					
